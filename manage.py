cf53112d2bf0747b4324c84b90f47e2c5b15b942
luan fonseca de farias <luan@luan-fonseca-de-fariass-iMac.local>
2012-09-03 09:57:54 -0300

    Adicionando a API do facebook

---
+++ manage.py
@@ -0,0 +1,11 @@
+from facebook import *
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Rodou maluco!111!!"
+
+if __name__ == "__main__":
+    app.run()
